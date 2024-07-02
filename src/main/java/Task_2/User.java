b11147da4725bd4ac4b112119c7f8a06c86c3245
kornejjj <165379747+kornejjj@users.noreply.github.com>
2024-07-02 20:41:41 +0200

    User.java

--- src/main/java/Task_2/User.java
+++ src/main/java/Task_2/User.java
@@ -1,2 +1,32 @@
-package Task_2;public class User {
+package Task_2;
+
+public class User {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Age: " + age;
+    }
 }
